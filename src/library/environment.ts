f15d309ffa3f0a3ee6a7898306b83610dee5d303
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-12 09:35:44 +0000

    Formatting

--- src/library/environment.ts
+++ src/library/environment.ts
@@ -5,6 +5,4 @@ export const bareFlyDomain = 'bare-domain.fly.dev'
 export const bareCustomDomain = 'bare-domain.co.uk'
 export const productionBaseURL = `https://${bareCustomDomain}`
 export const developmentBaseURL = 'http://localhost:3000'
-export const dynamicBaseURL = isProduction
-  ? productionBaseURL
-  : developmentBaseURL
+export const dynamicBaseURL = isProduction ? productionBaseURL : developmentBaseURL
