f01270a9b7602ce080121a1a71ae9a403302acf7
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-14 10:14:28 +0000

    Delete tailwindconfig.ts

--- tailwindconfig.ts
+++
@@ -1,5 +0,0 @@
-import type { Config } from 'tailwindcss'
-
-export default {
-  content: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}', './src/app/**/*.{ts,tsx}'],
-} satisfies Config
