fccf855d01d39ba2e4240473baf906902ec36662
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-29 10:46:04 +0000

    Create apiPaths.ts

---
+++ src/types/definitions/apiPaths.ts
@@ -0,0 +1,8 @@
+export const apiPaths = {
+  authentication: {
+    signIn: '/api/authentication/sign-in',
+    createAccount: '/api/authentication/create-account',
+    signOut: '/api/authentication/sign-out',
+    deleteAccount: '/api/authentication/delete-account',
+  },
+} as const
