b3cc799b70663c92b4c6fd8d3e63bc4b0df7b261
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-18 10:42:45 +0000

    Add no-console exception comment

--- src/library/environment/requireVariable.ts
+++ src/library/environment/requireVariable.ts
@@ -1,12 +1,13 @@
 export function requireVariable(name: string): string {
-  if (typeof window !== 'undefined') {
-    throw new Error('Browser attempting to import a private environment variable')
-  }
+	if (typeof window !== 'undefined') {
+		throw new Error('Browser attempting to import a private environment variable')
+	}
 
-  const value = process.env[name]
-  if (!value) {
-    console.error(`${name} missing`)
-    throw new Error(`${name} missing`)
-  }
-  return value
+	const value = process.env[name]
+	if (!value) {
+		// biome-ignore lint/suspicious/noConsole: <Using my logger here will cause a circular dependency>
+		console.error(`${name} missing`)
+		throw new Error(`${name} missing`)
+	}
+	return value
 }
