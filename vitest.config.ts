f94e577b59acc07cf70cb21a6f01c54e14f46e60
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-07 16:55:16 +0100

    Create vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import path from 'node:path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+	test: {
+		include: ['**/test.ts', '**/*.test.ts'],
+		exclude: ['**/node_modules/**', '.next/**'],
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+})
