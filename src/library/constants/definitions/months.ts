60f48da82b569d92522276b635347fa9ce510e60
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-27 10:08:12 +0100

    Update months.ts

--- src/library/constants/definitions/months.ts
+++ src/library/constants/definitions/months.ts
@@ -1,14 +1,14 @@
-export const months = {
-	_01january: 0,
-	_02february: 1,
-	_03march: 2,
-	_04april: 3,
-	_05may: 4,
-	_06june: 5,
-	_07july: 6,
-	_08august: 7,
-	_09september: 8,
-	_10october: 9,
-	_11november: 10,
-	_12december: 11,
-} as const
+import type { Month } from '@/types'
+
+export const january: Month = 0
+export const february: Month = 1
+export const march: Month = 2
+export const april: Month = 3
+export const may: Month = 4
+export const june: Month = 5
+export const july: Month = 6
+export const august: Month = 7
+export const september: Month = 8
+export const october: Month = 9
+export const november: Month = 10
+export const december: Month = 11
