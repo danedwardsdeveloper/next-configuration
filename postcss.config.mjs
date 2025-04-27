ed9b68119a8d1174f4899a9d0a12945d4f7cde56
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-27 10:09:11 +0100

    Update Tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,35 +1,8 @@
-// Old version!
 /** @type {import('postcss-load-config').Config} */
 const config = {
 	plugins: {
-		tailwindcss: {},
+		'@tailwindcss/postcss': {},
 	},
 }
 
 export default config
-
-/*
-UPDATED VERSION
-
-/** @type {import('postcss-load-config').Config} */
-/*
-const config = {
-	plugins: {
-		"@tailwindcss/postcss": {},
-	},
-};
-
-export default config;
-*/
-// This is the updated version - Friday 18 April 2025
-/*
-pnpm rm postcss-import autoprefixer postcss-nesting
-pnpm update tailwindcss@latest
-pnpm add @tailwindcss/postcss postcss
-
-Add
-@import "tailwindcss";
-to styles.tailwind.css
-
-Make sure you're not using @apply with custom classes - apply utility classes directly instead
-*/
