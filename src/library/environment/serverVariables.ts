f2abfe994c71faffe6eb49f0123821b9be0cdb5f
Dan Edwards <dan@shopsafe.io>
2025-07-07 18:36:20 +0100

    Delete serverSideBaseUrl

--- src/library/environment/serverVariables.ts
+++ src/library/environment/serverVariables.ts
@@ -1,11 +1,3 @@
-import { isProduction, productionBaseURL, developmentBaseURL } from './publicVariables'
 import { requireVariable } from './requireVariable'
 
 export const someVariable = requireVariable('SOME_VARIABLE')
-
-export const serverSideBaseUrl = isProduction
-	? process.env.VERCEL_URL
-		? `https://${process.env.VERCEL_URL}`
-		: productionBaseURL
-	: developmentBaseURL
-
