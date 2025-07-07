461a56fb22cb183bff4a67f58f7888ce66e29445
Dan Edwards <dan@shopsafe.io>
2025-07-07 18:37:06 +0100

    Remove clientSideBaseURL

--- src/library/environment/publicVariables.ts
+++ src/library/environment/publicVariables.ts
@@ -1,14 +1,16 @@
-import type { LogLevel } from '@/types'
+import type { LogLevel } from '@/types';
 
-export const isProduction = process.env.NODE_ENV === 'production'
-export const isDevelopment = process.env.NODE_ENV === 'development'
+export const isProduction = process.env.NODE_ENV === 'production';
+export const isDevelopment = process.env.NODE_ENV === 'development';
 
-export const siteIsLaunched = false
+export const bareDomain = 'bare-domain.vercel.app';
+export const productionBaseURL = `https://${bareDomain}`;
+export const developmentBaseURL = 'http://localhost:3000';
+export const clientSideBaseURL = isProduction
+	? productionBaseURL
+	: developmentBaseURL;
 
-export const bareDomain = 'bare-domain.fly.dev'
-export const productionBaseURL = `https://${bareDomain}`
-export const developmentBaseURL = 'http://localhost:3000'
-export const clientSideBaseURL = isProduction ? productionBaseURL : developmentBaseURL
-
-export const serverLogLevel: LogLevel = 'level5debug'
-export const browserLogLevel: LogLevel = isDevelopment ? 'level5debug' : 'level0none'
+export const serverLogLevel: LogLevel = 'level5debug';
+export const browserLogLevel: LogLevel = isDevelopment
+	? 'level5debug'
+	: 'level0none';
