871a142b7625ed6ba2690cce209cab9a0cbeb7f2
Dan Edwards <daniel.edwards96@yahoo.com>
2025-12-20 22:10:59 +0000

    Updates??

--- src/library/database/connection.ts
+++ src/library/database/connection.ts
@@ -1,11 +1,16 @@
+import { type NodePgDatabase, drizzle } from 'drizzle-orm/node-postgres'
 import { sql } from 'drizzle-orm'
-import { drizzle } from 'drizzle-orm/node-postgres'
 import * as schema from './schema'
 import logger from '../logger'
 import { isProduction, developmentDatabaseString } from '../environment/publicVariables'
 import { productionDatabaseString } from '../environment/serverVariables'
 
-export const database = drizzle(isProduction ? productionDatabaseString : developmentDatabaseString, { schema })
+export const database = drizzle(
+	isProduction // Doing it this way around ensures development is used in tests
+		? productionDatabaseString //
+		: developmentDatabaseString,
+	{ schema },
+)
 
 export async function testDatabaseConnection() {
 	try {
@@ -15,3 +20,6 @@ export async function testDatabaseConnection() {
 		logger.error('Database connection test failed:', error)
 	}
 }
+
+export type Database = NodePgDatabase<typeof schema>
+export type Transaction = Parameters<Parameters<typeof database.transaction>[0]>[0]
--- src/library/environment/publicVariables.ts
+++ src/library/environment/publicVariables.ts
@@ -1,7 +1,5 @@
-import type { LogLevel } from "@/types";
-
-export const isProduction = process.env.NODE_ENV === "production";
-export const isDevelopment = process.env.NODE_ENV === "development";
+export const isProduction = process.env.NODE_ENV === 'production'
+export const isDevelopment = process.env.NODE_ENV === 'development'
 
 /*
 Create a local database
@@ -11,14 +9,7 @@ CREATE DATABASE project_name;
 
 export const developmentDatabaseString = 'postgresql://localhost/next_configuration'
 
-export const bareDomain = "bare-domain.vercel.app";
-export const productionBaseUrl = `https://${bareDomain}`;
-export const developmentBaseUrl = "http://localhost:3000";
-export const dynamicBaseUrl = isProduction
-	? productionBaseUrl
-	: developmentBaseUrl;
-
-export const serverLogLevel: LogLevel = "level5debug";
-export const browserLogLevel: LogLevel = isDevelopment
-	? "level5debug"
-	: "level0none";
+export const bareDomain = 'bare-domain.vercel.app'
+export const productionBaseUrl = `https://${bareDomain}`
+export const developmentBaseUrl = 'http://localhost:3000'
+export const dynamicBaseUrl = isProduction ? productionBaseUrl : developmentBaseUrl
